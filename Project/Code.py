4c75f1581af65cd0b08191cadf00d4fb9fe8140a
Muhammad Sarim Raza <55577954+hackipie@users.noreply.github.com>
2022-05-21 21:45:16 +0500

    Update Code.py

--- Project/Code.py
+++ Project/Code.py
@@ -44,6 +44,18 @@ print("ACCURACCY BY NO SMOOTHING:- ",NoMNB*100)
 skf = StratifiedKFold(n_splits=3)
 for train, test in skf.split(X, y):
   print('train -  {}   |   test -  {}'.format(np.bincount(y[train]), np.bincount(y[test])))
+  
+#CONTRIBUTION OF SARIM RAZA
+#LAPLACE SMOOTHING
+clf = MultinomialNB(alpha=1)
+clf.fit(abs(t_train),y_train)
+clf.fit(abs(t_train),y_train)
+clf.predict(t_test)
+LPMNB=clf.score(t_test,y_test)
+print("ACCURACY BY LAPLACE SMOOTHING:- ",LPMNB*100)
+skf = StratifiedKFold(n_splits=3)
+for train, test in skf.split(X, y):
+  print('train -  {}   |   test -  {}'.format(np.bincount(y[train]), np.bincount(y[test])))
 
 #CONTRIBUTION OF MUHAMMAD HUZAIFA
 #LIDSTONE SMOOTHING
